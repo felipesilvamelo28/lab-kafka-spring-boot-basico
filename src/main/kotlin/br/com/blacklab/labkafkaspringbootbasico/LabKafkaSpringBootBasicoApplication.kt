c11473cdc821d3b69f5f7f081be0fe3c3ddfbba3
Felipe Melo <felipesilvamelo28@gmail.com>
2022-01-15 17:21:02 -0300

    criando projeto

---
+++ src/main/kotlin/br/com/blacklab/labkafkaspringbootbasico/LabKafkaSpringBootBasicoApplication.kt
@@ -0,0 +1,11 @@
+package br.com.blacklab.labkafkaspringbootbasico
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LabKafkaSpringBootBasicoApplication
+
+fun main(args: Array<String>) {
+	runApplication<LabKafkaSpringBootBasicoApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/blacklab/labkafkaspringbootbasico/LabKafkaSpringBootBasicoApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.blacklab.labkafkaspringbootbasico
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LabKafkaSpringBootBasicoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
