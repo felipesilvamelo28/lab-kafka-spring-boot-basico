26739b7f37ef40897fce1a7d226e39ac3aafc684
Felipe Melo <felipesilvamelo28@gmail.com>
2022-01-15 17:28:50 -0300

    criando initializer

--- src/main/kotlin/br/com/blacklab/labkafkaspringbootbasico/LabKafkaSpringBootBasicoApplication.kt
+++ src/main/kotlin/br/com/blacklab/labkafkaspringbootbasico/LabKafkaSpringBootBasicoApplication.kt
@@ -1,10 +1,20 @@
 package br.com.blacklab.labkafkaspringbootbasico
 
+import br.com.blacklab.labkafkaspringbootbasico.producer.MessageProducerImp
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class LabKafkaSpringBootBasicoApplication
+class LabKafkaSpringBootBasicoApplication(
+	val producer: MessageProducerImp
+): ApplicationRunner {
+	override fun run(args: ApplicationArguments?) {
+		val msg = "Mensagem Teste Black Lab"
+		producer.send(msg)
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<LabKafkaSpringBootBasicoApplication>(*args)
